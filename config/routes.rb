ca558e60b09b02c0d6c78905c58cb2b2f2839c6f
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-20 09:12:59 -0300

    expenses & category set up, migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :expenses
+  resources :categories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ db/migrate/20221220120749_create_expenses.rb
@@ -0,0 +1,10 @@
+class CreateExpenses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenses do |t|
+      t.string :title
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
