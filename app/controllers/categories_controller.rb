b3bfa1f3858fcf6f285038099832a70d9f5d7ccf
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-21 04:20:55 -0300

    add images to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -65,6 +65,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:name, :icon)
     end
 end
