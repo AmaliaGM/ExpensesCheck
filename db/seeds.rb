bd16aa55dab3901abec0e1d0821f828691b7cb0f
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-22 16:00:07 -0300

    fix application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Category.create(name: 'food')
-Category.create(name: 'clothes')
-Category.create(name: 'entretainment')
