4127697fc2aa9ebc9ede594c922d4e9a7f822112
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-22 10:32:31 -0300

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_20_213301) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_213301) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
