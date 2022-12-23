97a9a3311d70aa41cfab22d4d12ae860a8126d79
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-23 00:55:26 -0300

    fix linters

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-      
-    let(:valid_attributes) do
-      {
-        name: 'Groceries',
-        icon: 'GroceriesIcon',
-        id: '2'
-      }
-    end
-  
-    subject { Category.new valid_attributes }
-  
-    it 'is valid with a name' do
-      subject.name = "Groceries"
-      expect(subject).to be_valid
-    end
-  
-    it 'is valid with an icon' do
-      subject.icon = "GroceriesIcon"
-      expect(subject).to be_valid
-    end
-  
-    it 'is valid with an id' do
-      subject.id = '2'
-      expect(subject).to be_valid
-    end
+  let(:valid_attributes) do
+    {
+      name: 'Groceries',
+      icon: 'GroceriesIcon',
+      id: '2'
+    }
+  end
+
+  subject { Category.new valid_attributes }
+
+  it 'is valid with a name' do
+    subject.name = 'Groceries'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with an icon' do
+    subject.icon = 'GroceriesIcon'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with an id' do
+    subject.id = '2'
+    expect(subject).to be_valid
+  end
 end
-  
