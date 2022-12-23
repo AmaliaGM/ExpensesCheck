3aeaafda3d64865907ae338265bbb499c024ad31
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-23 01:42:55 -0300

    try to fix expense_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,11 +13,6 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
   it 'is not valid without an email' do
     subject.email = nil
     expect(subject).to_not be_valid
