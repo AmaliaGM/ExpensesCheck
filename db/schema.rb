2a7d8d704315ddc0c18f87d381b9895e04222704
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-25 17:11:11 -0300

    schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_235226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "icon"
-    t.integer "user_id"
     t.index ["user_id"], name: "index_categories_on_user_id", unique: true
   end
 
@@ -28,10 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_235226) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
-    t.integer "user_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
-    t.index ["user_id"], name: "index_expenses_on_user_id", unique: true
+    t.index ["user_id"], name: "index_expenses_on_user_id", unique: true, null: false
   end
 
   create_table "users", force: :cascade do |t|
