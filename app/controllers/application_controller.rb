a40b85d85f67ce5ed6d7f28bfc8f69bb72b929da
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-24 21:35:53 -0300

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [nuserame:])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
