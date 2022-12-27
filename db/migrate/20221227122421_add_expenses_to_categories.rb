72450af0635e50f0878ce214cf445a2baf11d06e
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-27 10:27:11 -0300

    delete add expenses to categories

--- db/migrate/20221227122421_add_expenses_to_categories.rb
+++
@@ -1,8 +0,0 @@
-class AddExpensesToCategories < ActiveRecord::Migration[7.0]
-  def change
-    create_table :categories_expenses, id: false do |t|
-      t.belongs_to :category
-      t.belongs_to :expense
-    end
-  end
-end
