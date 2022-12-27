5969f156812240c76f672608740ecefaccf4d18d
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-27 10:04:56 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_122421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "icon"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "categories_expenses", id: false, force: :cascade do |t|
