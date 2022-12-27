f7768f9ce588f83a231e775b1da62c86e741bc37
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-27 11:18:15 -0300

    drop migrations,create,migrate again

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_122421) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_235226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "icon"
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
-  end
-
-  create_table "categories_expenses", id: false, force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "expense_id"
-    t.index ["category_id"], name: "index_categories_expenses_on_category_id"
-    t.index ["expense_id"], name: "index_categories_expenses_on_expense_id"
   end
 
   create_table "expenses", force: :cascade do |t|
-    t.string "title", null: false
-    t.decimal "price", null: false
+    t.string "title"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
