a6d08e4985fe23ce3b8acc3cc106ef5e5296010e
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 14:40:52 -0300

    add unique

--- db/migrate/20221220213301_add_icon_to_category.rb
+++ db/migrate/20221220213301_add_icon_to_category.rb
@@ -1,6 +1,6 @@
 class AddIconToCategory < ActiveRecord::Migration[7.0]
     def change
-      add_column :categories, :icon, :string
-      add_column :categories, :user_id, :integer
+      add_column :categories, :icon, :string, unique: true
+      add_column :categories, :user_id, :integer, unique: true
     end
   end
--- db/migrate/20221224185645_add_user_id_to_expenses.rb
+++ db/migrate/20221224185645_add_user_id_to_expenses.rb
@@ -1,6 +1,6 @@
 class AddUserIdToExpenses < ActiveRecord::Migration[7.0]
   def change
     add_column :expenses, :user_id, :integer
-    add_index :expenses, :user_id
+    add_index :expenses, :user_id, unique: true
   end
 end
