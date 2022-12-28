cb6b720e94f3c9d72418cf98df5242d72c89cdd0
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 10:49:29 -0300

    update migrations

--- db/migrate/20221228132504_add_expense_to_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddExpenseToCategories < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :categories, :expense, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_132504) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_235226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_132504) do
     t.datetime "updated_at", null: false
     t.string "icon"
     t.integer "user_id"
-    t.bigint "expense_id", null: false
-    t.index ["expense_id"], name: "index_categories_on_expense_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -59,5 +57,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_132504) do
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "categories", "expenses"
 end
