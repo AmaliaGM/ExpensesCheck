26386b9f4a1a99ea322b837e7e857cdd36a9d618
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 11:59:26 -0300

    update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :expenses
+  has_and_belongs_to_many :expenses
 
   validates :name, presence: true
   validates :icon, :user_id, presence: true
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  has_and_belongs_to_many :categories
 
   validates :title, :price, :category_id, :user_id, presence: true
   validates_presence_of :category
