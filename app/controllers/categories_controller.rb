0ca4b55a638e4354e5c73f28709c454a69c40398
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 09:21:25 -0300

    categories_controller update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,13 +23,10 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     respond_to do |format|
-      if @category.save
+      
         format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    
     end
   end
 
