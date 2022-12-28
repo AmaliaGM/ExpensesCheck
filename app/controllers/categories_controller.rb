e9e581c977b723ed9b7f1a9cb9fbe9d7c306d818
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 12:31:38 -0300

    category update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = current_user.categories
+    @categories = Category.all
   end
 
   # GET /categories/1 or /categories/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories, dependent: :destroy
-  has_many :expenses, dependent: :destroy
+  has_many :categories
+  has_many :expenses
 
   validates :username, presence: true
 end
