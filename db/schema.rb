dbd0a02ad164ad1ad1e220a5ece8ed04a7c3a31c
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 02:37:47 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_211241) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_24_235226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_211241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "category_id"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
