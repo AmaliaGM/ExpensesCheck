62793a1a05812a3606c5eb6c314ce9d2960405e9
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 13:08:07 -0300

    controllers update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::Base
-  class ApplicationController < ActionController::Base
-    # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    protect_from_forgery with: :exception
-    before_filter :require_authentication
-  
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
-    end
-    helper_method :current_user
-  
-  
-    def require_authentication
-      if current_user
-        true
-      else
-        redirect_to new_session_path
-      end
+  before_action :authenticate_user!, except: %i[welcome about splash]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+  end
+  helper_method :current_user
+
+  def require_authentication
+    if current_user
+      true
+    else
+      redirect_to new_session_path
     end
   end
 end
