e715692a396e76a1211404369e9c447cd574d979
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-28 11:06:14 -0300

    update user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,16 +20,6 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
